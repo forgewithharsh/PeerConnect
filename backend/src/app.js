78fb16290539096379d06f6dc82f47940425cb93
Harsh Guleria <guleriah59@gmail.com>
2025-11-28 18:48:23 +0530

    setup basic routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,9 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
-app.use(cors())
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 const start = async () => {
   const connectionDb = await mongoose.connect(
