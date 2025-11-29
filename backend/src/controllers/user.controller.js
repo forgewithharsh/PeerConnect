a312eb3944bb1d11f639738858eb8377269e7963
Harsh Guleria <guleriah59@gmail.com>
2025-11-29 10:07:59 +0530

    setup: login controller and testing in thunder client

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,35 @@
-import { User } from "../models/user.model";
+import { User } from "../models/user.model.js";
 import httpStatus from "http-status";
 import bcrypt, { hash } from "bcrypt";
+import crypto from "crypto";
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please Provide" });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "User Not Found" });
+    }
+
+    if (bcrypt.compare(password, user.password)) {
+      let token = crypto.randomBytes(20).toString("hex");
+
+      user.token = token;
+      await user.save();
+      return res.status(httpStatus.OK).json({ token: token });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: `Something went wrong ${e}` });
+  }
+};
 
 const register = async (req, res) => {
   const { name, username, password } = req.body;
@@ -30,3 +59,5 @@ const register = async (req, res) => {
     res.json({ message: `Something went wrong ${error}` });
   }
 };
+
+export { login, register };
