149f9991d39f206602c59083926545f22d8468c9
Harsh Guleria <guleriah59@gmail.com>
2025-12-01 07:04:00 +0530

    setup vite again and react-router-dom

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>PeerConnect</h2>
+        </div>
+        <div className="navlist"></div>
+      </nav>
+    </div>
+  );
+}
+
+export default LandingPage;
